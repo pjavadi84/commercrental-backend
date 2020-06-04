f469b783480def4c9ad38c13011f76d3481dd340
github username <github email address>
2020-06-03 18:55:32 -0700

    create index and create method as well as strong params for equipment controller

--- app/controllers/api/v1/equipments_controller.rb
+++ app/controllers/api/v1/equipments_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::EquipmentsController < ApplicationController
+
+    def index
+        equipments = Equipment.all
+        # objective to render json format
+        render json: equipments
+    end
+
+    def create 
+        equipment = Equipment.new(equipment_params)
+    end
+
+    private 
+    def equipment_params 
+        params.require(equipment).permit(:name,:item_id,:category,:rental_start_date,:rental_end_date,:availability,:price_per_day,:price_per_hour,:price_per_month,:pickup,:delivery,:additional_comments)
+    end
 end
